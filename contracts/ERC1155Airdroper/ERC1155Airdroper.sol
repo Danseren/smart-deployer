9898fccac70af72575ee06669b4c5cbe2f22c76d
Danseren <etlendo@mail.ru>
2025-05-21 20:52:12 +0500

    docs: add comprehensive NatSpec comments for ERC1155Airdroper contract

--- contracts/ERC1155Airdroper/ERC1155Airdroper.sol
+++ contracts/ERC1155Airdroper/ERC1155Airdroper.sol
@@ -6,27 +6,51 @@ import "../UtilityContract/AbstractUtilityContract.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title ERC1155 Airdroper - Utility contract for batch ERC1155 token distribution
+/// @notice Utility contract for batch airdropping ERC1155 tokens
+/// @dev Inherits from AbstractUtilityContract and Ownable for deployment and access control
 contract ERC1155Airdroper is AbstractUtilityContract, Ownable {
+    /// @notice Constructor initializes the contract with the sender as the initial owner
     constructor() payable Ownable(msg.sender) {}
 
+    /// @notice Maximum number of tokens that can be airdropped in a single transaction
     uint256 public constant MAX_AIRDROP_BATCH_SIZE = 10;
 
+    /// @notice ERC1155 token contract to be used for airdrops
     IERC1155 public token;
+
+    /// @notice Treasury address holding the tokens for airdrop
     address public treasury;
 
+    /// @dev Flag to prevent multiple initializations
     bool private initialized;
 
+    /// @dev Error thrown when attempting to initialize the contract more than once
     error AlreadyInitialized();
+
+    /// @dev Error thrown when the number of receivers does not match other input arrays
     error RecieversLengthMismatch();
+
+    /// @dev Error thrown when the number of amounts does not match other input arrays
     error AmountsLengthMismatch();
+
+    /// @dev Error thrown when attempting to airdrop more tokens than the batch size limit
     error BatchSizeExceeded();
+
+    /// @dev Error thrown when the treasury has not approved the contract to transfer tokens
     error NeedToApproveTokens();
 
+    /// @notice Modifier to ensure the contract has not been initialized before
     modifier notInitialized() {
         require(!initialized, AlreadyInitialized());
         _;
     }
 
+    /// @notice Performs batch airdrop of ERC1155 tokens
+    /// @dev Transfers tokens from treasury to specified receivers
+    /// @param receivers Array of recipient addresses
+    /// @param amounts Array of token amounts to transfer
+    /// @param tokenIds Array of token IDs to transfer
     function airdrop(address[] calldata receivers, uint256[] calldata amounts, uint256[] calldata tokenIds)
         external
         onlyOwner
@@ -46,6 +70,8 @@ contract ERC1155Airdroper is AbstractUtilityContract, Ownable {
         }
     }
 
+    /// @inheritdoc IUtilityContract
+    /// @dev Initializes the contract with deploy manager, token, treasury, and owner
     function initialize(bytes memory _initData) external override notInitialized returns (bool) {
         (address _deployManager, address _token, address _treasury, address _owner) =
             abi.decode(_initData, (address, address, address, address));
@@ -61,6 +87,13 @@ contract ERC1155Airdroper is AbstractUtilityContract, Ownable {
         return true;
     }
 
+    /// @notice Generates initialization data for the contract
+    /// @dev Encodes initialization parameters for use with deploy manager
+    /// @param _deployManager Address of the deploy manager
+    /// @param _token Address of the ERC1155 token contract
+    /// @param _treasury Address of the treasury holding tokens
+    /// @param _owner Address of the contract owner
+    /// @return bytes Encoded initialization data
     function getInitData(address _deployManager, address _token, address _treasury, address _owner)
         external
         pure
